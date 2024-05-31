7ae3f50d406a4ee15d1096f22eb419b474a4312f
JeffMuter <muterjeffery@gmail.com>
2024-05-30 23:42:50 -0400

    fixed the session issue. Mismatched string in middleware to setSession. God level programming this late at night. lets go!

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -9,10 +9,9 @@ import (
 
 func main() {
 
-	// fmt.Printf("All users: %v", models.GetUsers())
 	mux := routes.Router()
 
-	log.Printf("Started server on 8080:\nhttp://localhost:8080")
+	log.Printf("\nhttp://localhost:8080")
 
 	err := http.ListenAndServe(":8080", mux)
 
--- internal/middleware/middleware.go
+++ internal/middleware/middleware.go
@@ -4,7 +4,7 @@ import "net/http"
 
 func AuthMiddleware(handler http.HandlerFunc) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		_, err := r.Cookie("session_id")
+		_, err := r.Cookie("session_token")
 		if err != nil {
 			http.Redirect(w, r, "/login", http.StatusFound)
 			return
